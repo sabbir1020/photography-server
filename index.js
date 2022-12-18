3355765261b68d546491c628fdd46cc161600c3b
sabbir1020 <gazisabbir924@gamil.com>
2022-12-19 00:22:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleWare
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.czgvc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+console.log("connected database");
+
+app.get("/", (req, res) => {
+  res.send("photography server is running");
+});
+
+app.listen(port, () => {
+  console.log(`photography server is running ${port}`);
+});
